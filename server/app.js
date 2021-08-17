fd5560999f0645ac56ad1faf1dfd8799f428f5d4
Daniil <a336nm@gmail.com>
2021-08-17 15:45:27 +0300

    deploy configs modified

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require('cors')
-
 const fileUpload = require('express-fileupload')
 const path = require('path')
 
@@ -16,11 +15,34 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(express.json())
-
 app.use(fileUpload({}))
-app.use('/api', router)
-app.use(errorHandler)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken())
+    res.sendFile(
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
+    )
+})
+  router.use(express.static(path.resolve("../client/build")))
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
+    );
+  })
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
+
+app.use('/api', router)
 app.use(errorHandler) //Обработка ошибок, последний mw!
 
 const start = async () => {
@@ -28,11 +50,10 @@ const start = async () => {
     await sequelize.authenticate()
     await sequelize.sync()
     app.listen(PORT,
-      // () => console.log(`Server started on port ${PORT}`)
+      () => console.log(`Server started on port ${PORT}`),
     )
   } catch (error) {
-    // console.log(new Error(error))
-    return new Error(error)
+    console.log(new Error(error))
   }
 }
 
--- server/db.js
+++ server/db.js
@@ -1,7 +1,10 @@
 const {Sequelize} = require("sequelize")
 
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT
-})
+const prodConfig = process.env.DATABASE_URL
+const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+module.exports = new Sequelize(
+  process.env.NODE_ENV === 'production' ?
+    prodConfig :
+    devConfig
+  )
