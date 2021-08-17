9335e75eb934653aa402b0aeb710613fd83a5b05
Daniil <a336nm@gmail.com>
2021-08-18 00:08:14 +0300

    db config v8 (sequelize)

--- server/db.js
+++ server/db.js
@@ -4,6 +4,9 @@ const prodConfig = {
   connectionString: process.env.DATABASE_URL,
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 }
 
 const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
