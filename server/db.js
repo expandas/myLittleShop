de9f99a90ead9bfa88f0d332d8f3afd9d0b60dbf
Daniil <a336nm@gmail.com>
2021-08-17 23:23:28 +0300

    db config v3 (sequelize)

--- server/db.js
+++ server/db.js
@@ -1,26 +1,19 @@
-if (process.env.NODE_ENV === 'production') {
-  const {Client} = require('pg')
+const {Sequelize} = require("sequelize")
 
-  module.exports = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-} else {
-  const {Sequelize} = require("sequelize")
-
-  const prodConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-    dialect: 'postgres'
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
   }
-  const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-
-  module.exports = new Sequelize(
-    process.env.NODE_ENV === 'production' ?
-      prodConfig :
-      devConfig
-  )
 }
+
+const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+module.exports = new Sequelize(
+  process.env.NODE_ENV === 'production' ?
+    prodConfig :
+    devConfig
+)
 
