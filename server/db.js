e314e10f0d8df52d2808ba8e90ef1eb531a18ecd
Daniil <a336nm@gmail.com>
2021-08-18 10:47:15 +0300

    db config v13 (sequelize)

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require("sequelize")
 
-const prodConfig = {
-  connectionString: process.env.DATABASE_URL,
+const prodConfig = process.env.DATABASE_URL
   // dialect: 'postgres',
   // protocol: 'postgres',
   // dialectOptions: {
@@ -9,7 +8,7 @@ const prodConfig = {
   //   rejectUnauthorized: false
   // }
   // }
-}
+
 
 const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
